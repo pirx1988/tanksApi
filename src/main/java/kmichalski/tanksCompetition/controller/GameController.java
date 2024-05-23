136396cd3a37d6c84746cec4364fe4f358bc10c8
INTENCER\k.michalski <krzysztof.michalski@intive.com>
2024-05-23 17:09:18 +0200

    add validation rules

--- src/main/java/kmichalski/tanksCompetition/controller/GameController.java
+++ src/main/java/kmichalski/tanksCompetition/controller/GameController.java
@@ -1,22 +1,30 @@
 package kmichalski.tanksCompetition.controller;
 
+import jakarta.validation.Valid;
 import kmichalski.tanksCompetition.dto.UpdateGameDto;
 import kmichalski.tanksCompetition.model.Game;
 import kmichalski.tanksCompetition.service.GameService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 public class GameController {
     private final GameService gameService;
+
     @PostMapping("/create-new-game")
     public ResponseEntity<Game> createGame(@RequestBody Game game) {
         Game createdGame = gameService.createNewGame(game);
@@ -30,6 +38,7 @@ public class GameController {
         // Return ResponseEntity with 201 Created status code and location header
         return ResponseEntity.created(location).body(createdGame);
     }
+
     @GetMapping("/games")
     public ResponseEntity<List<Game>> fetchGames() {
         return ResponseEntity.ok().body(gameService.findGames());
@@ -37,8 +46,7 @@ public class GameController {
 
     // Start game
     @PutMapping("/update-game/{gameId}")
-    public ResponseEntity<Game> updateGame(@RequestBody UpdateGameDto updateGameDto, @PathVariable UUID gameId) {
+    public ResponseEntity<Game> updateGame(@Valid @RequestBody UpdateGameDto updateGameDto, @PathVariable UUID gameId) {
         return ResponseEntity.ok().body(gameService.updateGame(gameId, updateGameDto));
     }
-
 }
--- src/main/java/kmichalski/tanksCompetition/dto/UpdateGameDto.java
+++ src/main/java/kmichalski/tanksCompetition/dto/UpdateGameDto.java
@@ -1,5 +1,9 @@
 package kmichalski.tanksCompetition.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import kmichalski.tanksCompetition.model.GameStatus;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +13,10 @@ import java.util.UUID;
 @Data
 @Builder
 public class UpdateGameDto {
+    @NotNull(message = "Game status must not be null")
     GameStatus gameStatus;
+    @Min(value = 0, message = "First player Game score must be bigger or equal than zero")
     int firstPlayerScore;
+    @Min(value = 0, message = "Second player Game score must be bigger or equal than zero")
     int SecondPlayerScore;
 }
